e24fd1ee37cfc3218fd7828c0ddc435f22a2be13
miguelnery <nery.mik@gmail.com>
2023-11-21 11:38:00 -0300

    base project

---
+++ BasicNetwork/BasicNetwork/BasicNetwork.h
@@ -0,0 +1,18 @@
+//
+//  BasicNetwork.h
+//  BasicNetwork
+//
+//  Created by mik on 21/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BasicNetwork.
+FOUNDATION_EXPORT double BasicNetworkVersionNumber;
+
+//! Project version string for BasicNetwork.
+FOUNDATION_EXPORT const unsigned char BasicNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicNetwork/PublicHeader.h>
+
+
